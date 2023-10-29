cadcf42ade5b4c8cb59f2a0d0054f639f44a4fcc
nikita <nikyar880@gmail.com>
2023-10-29 22:29:23 +0200

    added new branch playright

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,6 @@ module.exports = defineConfig({
     },
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'http://qauto.forstudy.space',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
