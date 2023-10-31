702e7c4079d24e64e18465ea13b3a3f67e5ee48d
nikita <nikyar880@gmail.com>
2023-10-31 12:44:58 +0200

    hello

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
+// Stas help
   /* Configure projects for major browsers */
   projects: [
     {
